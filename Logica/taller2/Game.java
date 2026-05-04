ca144eeda548c9431aecbad1347fe4bb6d72ff04
EmiToro <emiliano.toro@alumnos.ucn.cl>
2026-05-04 18:27:44 -0400

    Avance Game.java, creacion de objetos y termine Pokemon.java
    termine los objetos faltantes que serian Gimnasio.java, Habitat.java y Pokemon.java, ademas de que avance en el menu, cree las funciones y termine el Objeto Pokemon.java

--- Logica/taller2/Game.java
+++ Logica/taller2/Game.java
@@ -1,12 +1,100 @@
 package taller2;
 
+
+import java.util.*;
+import java.io.*;
+
+
 public class Game {
+	private static ArrayList<Pokemon> Pokedex = new ArrayList<Pokemon>();
 
 	public static void main(String[] args) {
+		Scanner esc = new Scanner(System.in);
 		System.out.println("--- Bienvenido al menu de Pokemon!! ---/n");
+		boolean salir = false;
 		
-		
+		while(!salir) {
+			System.out.println("1) Revisar equipo.");
+			System.out.println("2) Salir a capturar.");
+			System.out.println("3) Acceso al PC (cambiar Pokémon del equipo).");
+			System.out.println("4) Retar un gimnasio.");
+			System.out.println("5) Desafío al Alto Mando.");
+			System.out.println("6) Curar Pokémon.");
+			System.out.println("7) Guardar.");
+			System.out.println("8) Guardar y Salir.");
+			System.out.println("/n -Porfavor ingrese su opcion: ");
+			int opcion = esc.nextInt(); esc.nextLine();
+			
+			while(opcion <= 0 || opcion > 8) {
+				System.out.println("ERROR, porfavor ingrese una opcion valida: ");
+				opcion = esc.nextInt(); esc.nextLine();
+			}
+			
+			switch(opcion) {
+				case 1:
+					revisarEquipo();
+					break;
+				case 2:
+					salirCapturar();
+					break;
+				case 3:
+					accesoPC();
+					break;
+				case 4:
+					retarGym();
+					break;
+				case 5:
+					desafiarAltoMando();
+					break;
+				case 6:
+					curarPkm();
+					break;
+				case 7:
+					Guardar();
+					break;
+				case 8:
+					Guardar();
+					salir = true;
+					break;
+			}
+		}
+	}
 
+	
+	
+	private static void Guardar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void curarPkm() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void desafiarAltoMando() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void retarGym() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void accesoPC() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void salirCapturar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void revisarEquipo() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
---
+++ Logica/taller2/Pokemon.java
@@ -0,0 +1,92 @@
+package taller2;
+
+import java.util.*;
+
+public class Pokemon {
+	private String nombre;
+	private String habitat;
+	private double porcentajeAparicion;
+	private int vida;
+	private int ataque;
+	private int defensa;
+	private int ataqueEspecial;
+	private int defensaEspecial;
+	private int velocidad;
+	private String tipo;
+	private List<Habitat> habitats;
+	
+	public Pokemon(String nombre, String habitat, double porcentajeAparicion, int vida, int ataque, int defensa,
+			int ataqueEspecial, int defensaEspecial, int velocidad, String tipo) {
+		super();
+		this.nombre = nombre;
+		this.habitat = habitat;
+		this.porcentajeAparicion = porcentajeAparicion;
+		this.vida = vida;
+		this.ataque = ataque;
+		this.defensa = defensa;
+		this.ataqueEspecial = ataqueEspecial;
+		this.defensaEspecial = defensaEspecial;
+		this.velocidad = velocidad;
+		this.tipo = tipo;
+		this.habitats = new ArrayList<Habitat>();
+		
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getHabitat() {
+		return habitat;
+	}
+
+	public double getPorcentajeAparicion() {
+		return porcentajeAparicion;
+	}
+
+	public int getVida() {
+		return vida;
+	}
+
+	public int getAtaque() {
+		return ataque;
+	}
+
+	public int getDefensa() {
+		return defensa;
+	}
+
+	public int getAtaqueEspecial() {
+		return ataqueEspecial;
+	}
+
+	public int getDefensaEspecial() {
+		return defensaEspecial;
+	}
+
+	public int getVelocidad() {
+		return velocidad;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public List<Habitat> getHabitats() {
+		return habitats;
+	}
+
+	public void addHabitats(Habitat h) {
+		this.habitats.add(h);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
