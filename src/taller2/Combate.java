ae711727d972a75adf37d29ed15ae2ad119724c1
Bunnyybundy <cferap14@gmail.com>
2026-05-04 22:51:59 -0400

    clase nueva

--- src/taller2/Combate.java
+++ src/taller2/Combate.java
@@ -1,5 +1,13 @@
 package taller2;
 
 public class Combate {
-
+	private Pokemon atacante;
+	private Pokemon defensor;
+	
+	public Combate(Pokemon atacante, Pokemon defensor) {
+		this.atacante = atacante;
+		this.defensor = defensor;
+	}
+	
+	
 }
